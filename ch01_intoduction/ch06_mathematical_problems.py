a26836580c2170528c81f1e48a61adc97f8cc971
Libioulle <darwin.libioulle@cgi.com>
2022-09-30 09:41:36 +0200

    add exercices

--- ch01_intoduction/ch06_mathematical_problems.py
+++ ch01_intoduction/ch06_mathematical_problems.py
@@ -22,13 +22,50 @@ def flip_horizontally(values):
 def get_dimension(values2dim):
     if isinstance(values2dim,list):
         print("Manual array")
+        print("Using manually")
         return (len(values2dim),len(values2dim[0]))
 
     if isinstance(values2dim,np.ndarray):
         print("Numpy array")
+        print("Usin numpy dimension")
         return values2dim.shape
 
     raise ValueError("unsupported type",type(values2dim))
+
+def swap(values,first,second):
+    buffer = values[first]
+    values[first]=values[second]
+    values[second] = buffer
+
+    return values
+
+
+
+def flip_horizontally_v2(values2dim):
+    max_y,max_x = get_dimension(values2dim)
+    print("max_y",max_y)
+    print("max_y",max_y)
+
+    for y in range(max_y):
+        print("y",y)
+        row = values2dim[y]
+        print("row",row)
+        for x in range(max_x // 2):
+            print("x",x)
+            print("max_x // 2" ,max_x // 2)
+            swap(row,x,max_x - x - 1)
+
+def flip_vertically_just_for_lists(values2dim):
+    max_y, _= get_dimension(values2dim)
+    print("max_y",max_y)
+    print("max_y//2",max_y // 2)
+    for y in range(max_y // 2):
+        print("y",y)
+        results =swap(values2dim,y,max_y - y -1)
+
+    return results
+
+
 
 
 
@@ -36,7 +73,15 @@ def get_dimension(values2dim):
 if __name__ == "__main__":
     # print(odd_numbers_first([2, 4, 6, 8, 1]))
     # print(flip_horizontally([1,2,3,4]))
-    a2D = np.array([[1,2],[3,2]])
-    array =[[1, 2, 3], [4, 5, 6],[7, 8, 9]]
+    a2D = np.array([[1,2,4],
+                    [3,2,7],
+                    [4,7,3],
+                    ])
 
-    print(get_dimension(a2D))
+    array =[[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
+
+    # print(get_dimension(a2D))
+    # print(flip_horizontally_v2(a2D))
+    print(flip_vertically_just_for_lists(a2D))
